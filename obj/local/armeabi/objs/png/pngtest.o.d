557f66904001d280961e86d8929750fd6a7f5848
fishle <fishle2016@qq.com>
2017-03-23 09:21:14 +0800

    delete some useless files

--- obj/local/armeabi/objs/png/pngtest.o.d
+++
@@ -1,8 +0,0 @@
-/Users/lockyyu/Desktop/workspace_qzone/others/libpng-android-ndk/obj/local/armeabi/objs/png/pngtest.o: \
-  pngtest.cpp png.h pnglibconf.h pngconf.h
-
-png.h:
-
-pnglibconf.h:
-
-pngconf.h:
--- obj/local/mips64/objs/png/pngtest.o.d
+++
@@ -1,8 +0,0 @@
-/Users/lockyyu/Desktop/workspace_qzone/others/libpng-android-ndk/obj/local/mips64/objs/png/pngtest.o: \
-  pngtest.cpp png.h pnglibconf.h pngconf.h
-
-png.h:
-
-pnglibconf.h:
-
-pngconf.h:
